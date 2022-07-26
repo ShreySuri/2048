07fbf5957db0b145292d219292201e2d1c1bea4d
ShreySuri <shreysuri5@gmail.com>
2022-07-25 21:48:24 -0500

    Update 2048_2.py

--- 2048_2.py
+++ 2048_2.py
@@ -1,5 +1,13 @@
 import random
 
+def base_4(int_1):
+	rem = int_1 % 4
+	quotient = int((int_1 - rem)/4)
+	final_int = 10 * quotient + rem
+	return(final_int)
+
+
+
 master_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 x = random.randint(0, 159):
@@ -7,4 +15,19 @@ y = x % 16
 z = int((x - y)/16)
 
 if z == 0:
+	master_list[y] = 4
+else:
+	master_list[y] = 2
+
+row_1 = []
+row_2 = []
+row_3 = []
+row_4 = []
+
+column_1 = []
+column_2 = []
+column_3 = []
+column_3 = []
+
+
 
